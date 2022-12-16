c55f898cb7cd23c50d678b67e934e047a185c8d5
Gael Mukendi Kabongo <111666680+gmka98@users.noreply.github.com>
2022-12-16 18:13:23 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,66 +1,87 @@
-const { render } = require('ejs');
 const express = require('express');
 const router = express.Router();
-const User = require("../models/user.js")
-
+const User = require("../models/user");
+const bcrypt = require('bcrypt');
+const passport = require('../models/config/passport');
 //login handle
-router.get('/login',(req,res) =>{
+router.get('/login',(req,res)=>{
     res.render('login');
 })
-router.get('/register',(req,res) =>{
+router.get('/register',(req,res)=>{
     res.render('register')
-})
+    })
 //Register handle
-router.post('register',(req,res) =>{
+router.post('/login',(req,res,next)=>{
+    passport.authenticate('local',{
+        successRedicrect : '/dashboard',
+        failureRedirect : '/users/login',
+        failureFlash : true,
+    }) (req,res,next);
+  })
+  //register post handle
+  router.post('/register',(req,res)=>{
     const {name,email, password, password2} = req.body;
     let errors = [];
-    console.log('Name' + name + 'email :' + email +'pass:' + password);
+    console.log(' Name ' + name+ ' email :' + email+ ' pass:' + password);
     if(!name || !email || !password || !password2) {
         errors.push({msg : "Please fill in all fields"})
- }
-//check if match 
-if(password !== password2) {
-    errors.push({msg : "password don't match"});
-}
-
-//check if password is more than 6 characters
-if(password.lenght < 6){
-    errors.push({msg : 'password atleast 6 characters'})
-}
-if(errors.lenght > 0) {
+    }
+    //check if match
+    if(password !== password2) {
+        errors.push({msg : "passwords dont match"});
+    }
+    
+    //check if password is more than 6 characters
+    if(password.length < 6 ) {
+        errors.push({msg : 'password atleast 6 characters'})
+    }
+    if(errors.length > 0 ) {
     res.render('register', {
-        errors: errors,
+        errors : errors,
         name : name,
         email : email,
         password : password,
         password2 : password2})
-    }
-    else{
+     } else {
         //validation passed
-        User.findOne({email : email}).exec((err,user) =>{
-            console.log(user);
-            if(user) {
-                errors.push({msg: 'email already registered'});
-                render(res,errors,name,email,password,password2);
-
-            }
-            else{
-                const newUser = new User({
-                    name : name,
-                    email : email,
-                    password : password
-
-                });
-            }
-        })
+       User.findOne({email : email}).exec((err,user)=>{
+        console.log(user);   
+        if(user) {
+            errors.push({msg: 'email already registered'});
+            res.render('register',{errors,name,email,password,password2})  
+           } else {
+            const newUser = new User({
+                name : name,
+                email : email,
+                password : password
+            });
+    
+            //hash password
+            bcrypt.genSalt(10,(err,salt)=> 
+            bcrypt.hash(newUser.password,salt,
+                (err,hash)=> {
+                    if(err) throw err;
+                        //save pass to hash
+                        newUser.password = hash;
+                    //save user
+                    newUser
+                    .save()
+                    .then((value)=>{
+                        console.log(value)
+                        req.flash('succes_msg','You have now registered!')
+                    res.redirect('/users/login');
+                    })
+                    .catch(value=> console.log(value));
+                      
+                }));
+             }
+       })
     }
-}
-)
-router.post('/login',(req,res,next) =>{
-
-})
+    })
 //logout
-router.get('/logout',(req,res) =>{
-
+router.get('/logout',(req,res)=>{
+ req.logout();
+req.flash('succes_msg','Now logged out');
+res.redirect('/users/login');
 })
-module.exports = router;
+module.exports  = router;   
