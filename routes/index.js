189491bd624f829aa7b7151d722e1efe61122ba2
Gael Mukendi Kabongo <111666680+gmka98@users.noreply.github.com>
2022-12-16 18:13:00 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const {ensureAuthenticated} = require("../config/auth.js")
 //login page
 router.get('/', (req, res) =>{
     res.render('welcome');
@@ -9,4 +10,9 @@ router.get('/register', (req,res) =>{
     res.render('register');
 })
 
+router.get('/dashboard', ensureAuthenticated,(req,res) =>{
+    res.render('dashboard',{
+        user: req.user
+    });
+})
 module.exports = router;
